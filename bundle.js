bf088e07d44567a95376e76b602dbbde7d808087
William Lim <manvinedi@gmail.com>
2017-08-09 13:17:54 +0800

    fixed broken image paths

--- bundle.js
+++ bundle.js
@@ -22870,7 +22870,7 @@
 	        _react2.default.createElement(
 	          'a',
 	          { tabIndex: '0' },
-	          _react2.default.createElement('img', { src: '/src/assets/art/jp-chrysanthemum.png', alt: '', height: '60px' }),
+	          _react2.default.createElement('img', { src: 'src/assets/art/jp-chrysanthemum.png', alt: '', height: '60px' }),
 	          _react2.default.createElement(
 	            'span',
 	            null,
@@ -23006,7 +23006,7 @@
 	        { className: 'search-bar__input-container' },
 	        _react2.default.createElement('input', { type: 'text', name: ' value=', tabIndex: '0' })
 	      ),
-	      _react2.default.createElement('img', { src: '/src/assets/simple-images/mag-glass-plain.svg', alt: 'search', tabIndex: '0' })
+	      _react2.default.createElement('img', { src: 'src/assets/simple-images/mag-glass-plain.svg', alt: 'search', tabIndex: '0' })
 	    ),
 	    _react2.default.createElement(
 	      'div',
--- src/components/search-bar/search_bar.js
+++ src/components/search-bar/search_bar.js
@@ -7,7 +7,7 @@ const SearchBar = (props) => {
         <div className='search-bar__input-container'>
           <input type='text' name=' value=' tabIndex='0' />
         </div>
-        <img src={'/src/assets/simple-images/mag-glass-plain.svg'} alt={'search'} tabIndex='0' />
+        <img src={'src/assets/simple-images/mag-glass-plain.svg'} alt={'search'} tabIndex='0' />
       </div>
       <div className='search-bar__back'>
         <a className='search-bar__back-button' tabIndex='0'>Back to Translator</a>
